a08dd851075e944ae5db7eab981f3ce76de6ad20
savan_kansagra <kansagrasavan110@gmail.com>
2021-10-23 23:06:26 +0530

    react hook use state demostated

--- myfirstapp/src/components/Expense/ExpenseItem.js
+++ myfirstapp/src/components/Expense/ExpenseItem.js
@@ -1,12 +1,18 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import React, { useState } from 'react';
 
 function ExpenseItem(props) {
 
     const expenseDate = props.date;
-    const expense_title = props.title;
     const expense_price = props.amount;
+
+    const [expense_title, setExpense_title] = useState(props.title);
+
+    const clickHandler = () => {
+        setExpense_title("Updated");    
+    };
 
     return (
         <Card className="expense-item">
@@ -15,6 +21,7 @@ function ExpenseItem(props) {
                 <h4>{expense_title}</h4>
                 <div className="expense-item__price">{expense_price}</div>
             </div>
+            <button onClick={clickHandler}>Change title!</button>
         </Card>
     );
 }
